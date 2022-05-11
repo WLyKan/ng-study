af21207850feb17eb2f10a5ebbfe535c5f50868d
King <wyk17@qq.com>
2022-05-11 17:50:51 +0800

    feat: 解决 html template问题

--- src/legacy/phone/phone-list.component.ts
+++ src/legacy/phone/phone-list.component.ts
@@ -1,9 +1,10 @@
 import { PhoneService } from 'app/core/services/phone.service'
 
-import template from './phone-list.component.html'
+// import template from './phone-list.component.html'
 
 angular.module('app').component('phoneList', {
-  template,
+  template: require('html-loader!./phone-list.component.html'),
+  // templateUrl: require('./phone-list.component.html'), // template 也可以加载到
   controller: [
     'phone',
     function(phone: PhoneService) {
