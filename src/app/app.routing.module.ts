0ec216a06ffa0a36131da1243590ce7260b829f7
King <wyk17@qq.com>
2022-05-11 18:10:31 +0800

    feat: update to v11

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,27 +3,30 @@ import { RouterModule } from '@angular/router'
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'console',
-      },
-      {
-        path: 'console',
-        loadChildren: () =>
-          import('./console/console.module').then(m => m.ConsoleModule),
-      },
-      {
-        path: 'landing',
-        loadChildren: () =>
-          import('./landing/landing.module').then(m => m.LandingModule),
-      },
-      {
-        path: '**',
-        redirectTo: 'console',
-      },
-    ]),
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'console',
+        },
+        {
+          path: 'console',
+          loadChildren: () =>
+            import('./console/console.module').then(m => m.ConsoleModule),
+        },
+        {
+          path: 'landing',
+          loadChildren: () =>
+            import('./landing/landing.module').then(m => m.LandingModule),
+        },
+        {
+          path: '**',
+          redirectTo: 'console',
+        },
+      ],
+      { relativeLinkResolution: 'legacy' },
+    ),
   ],
 })
 export class AppRoutingModule {}
--- src/app/console/home/home.component.spec.ts
+++ src/app/console/home/home.component.spec.ts
@@ -1,29 +1,40 @@
-import { TestBed, async } from '@angular/core/testing'
+import { TestBed, waitForAsync } from '@angular/core/testing'
 
 import { HomeComponent } from './home.component'
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-    }).compileComponents()
-  }))
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(HomeComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app).toBeTruthy()
-  }))
-  it(`should have as title 'ng-study'`, async(() => {
-    const fixture = TestBed.createComponent(HomeComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('ng-study')
-  }))
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(HomeComponent)
-    fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent.trim()).toEqual(
-      'Welcome to ng-study! Go to Phones',
-    )
-  }))
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomeComponent],
+      }).compileComponents()
+    }),
+  )
+  it(
+    'should create the app',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(HomeComponent)
+      const app = fixture.debugElement.componentInstance
+      expect(app).toBeTruthy()
+    }),
+  )
+  it(
+    `should have as title 'ng-study'`,
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(HomeComponent)
+      const app = fixture.debugElement.componentInstance
+      expect(app.title).toEqual('ng-study')
+    }),
+  )
+  it(
+    'should render title in a h1 tag',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(HomeComponent)
+      fixture.detectChanges()
+      const compiled = fixture.debugElement.nativeElement
+      expect(compiled.querySelector('h1').textContent.trim()).toEqual(
+        'Welcome to ng-study! Go to Phones',
+      )
+    }),
+  )
 })
